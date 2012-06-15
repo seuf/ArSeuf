130b7f17a37590909de4fd0cf4aa49d4c0ff9c80
Thierry <thierry@thierry-laptop.(none)>
2012-06-15 11:12:22 +0200

    Moved IO Pin declaration into IO_Pin.h

---
+++ ar_seuf/IO_Pin.h
@@ -0,0 +1,20 @@
+#ifndef __IO_PIN_H__
+#define __IO_PIN_H__
+
+
+//MOTOR PIN
+#define MOTOR_LEFT_PWM 2
+#define MOTOR_LEFT_DIR 3
+#define MOTOR_RIGHT_PWM 4
+#define MOTOR_RIGHT_DIR 5
+//PIEZO ELEMENT
+#define ZIK 6
+// Multicolor LED
+#define RED_LED_PIN  7
+#define GREEN_LED_PIN 8
+#define BLUE_LED_PIN 9
+#define SERVO_PIN 10
+//Ping Sonar
+#define PING_PIN 11
+
+#endif
