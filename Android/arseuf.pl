d0bba812b8c21eac32bebb8ff1cd22e65cbc7549
Seuf <seuf76@gmail.com>
2012-06-15 11:17:33 +0200

    Moved script 4 android into android directory

---
+++ Android/arseuf.pl
@@ -0,0 +1,20 @@
+
+use strict;
+use warnings;
+use HTTP::Tiny;
+use Data::Dumper;
+use Android;
+
+my $droid = Android->new();
+
+$droid->webViewShow('file:///sdcard/sl4a/scripts/arseuf.html');
+while (1) {
+  #my $res = $droid->waitForEvent('action');
+  my $res = $droid->eventWait();
+ 
+  print Dumper($res); 
+  my $cmd = $res->{result}->{data};
+   
+#  my $response = HTTP::Tiny->new->get('http://192.168.30.15/'.$cmd);
+#  $droid->makeToast("Error") unless $response->{success};
+}
